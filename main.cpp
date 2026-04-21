c89a124b6b0fafedc1b43476b81c9f07d02dc41e
Awleenn <15908@ait.nsw.edu.au>
2026-04-21 18:08:35 +1000

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,22 +4,38 @@
 
 using namespace std;
 
-// Book Class Definition [cite: 18]
+// Author Class
+class Author {
+private:
+    string name;
+
+public:
+    Author() : name("") {}
+
+    Author(string n) : name(n) {}
+
+    void setName(string n) {
+        name = n;
+    }
+
+    string getName() const {
+        return name;
+    }
+};
+
+// Book Class
 class Book {
 private:
-    // Attributes [cite: 19-24]
     string title;
-    string author;
+    Author author; // Now using Author class
     string isbn;
     bool isAvailable;
     string dateAdd;
 
 public:
-    // Default constructor
-    Book() : title(""), author(""), isbn(""), isAvailable(true), dateAdd("") {}
+    Book() : title(""), author(), isbn(""), isAvailable(true), dateAdd("") {}
 
-    // setBookDetails method [cite: 28]
-    void setBookDetails(string t, string a, string i, string d, bool avail = true) {
+    void setBookDetails(string t, Author a, string i, string d, bool avail = true) {
         title = t;
         author = a;
         isbn = i;
@@ -27,33 +43,31 @@ public:
         isAvailable = avail;
     }
 
-    // displayBookDetails method [cite: 29]
     void displayBookDetails() const {
-        cout << left << setw(10) << isbn 
-             << setw(25) << title 
-             << setw(20) << author 
+        cout << left << setw(10) << isbn
+             << setw(25) << title
+             << setw(20) << author.getName()
              << (isAvailable ? "Available" : "Borrowed") << endl;
     }
 
-    // borrowBook method [cite: 30, 33]
     bool borrowBook() {
         if (isAvailable) {
             isAvailable = false;
             return true;
         }
-        return false; // Validates availability before borrowing [cite: 33]
+        return false;
     }
 
-    // returnBook method [cite: 31]
     void returnBook() {
         isAvailable = true;
     }
 
-    // sortBookData method (Sorting by ISBN) 
-    string getISBN() const { return isbn; }
+    string getISBN() const {
+        return isbn;
+    }
 };
 
-// Global sort function for the array 
+// Sorting function (Bubble Sort)
 void sortBookData(Book books[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -65,18 +79,24 @@ void sortBookData(Book books[], int n) {
 }
 
 int main() {
-    // Task 2: Implementation [cite: 42, 43]
-    // Initialize an array with 5 books [cite: 35]
     const int SIZE = 5;
     Book library[SIZE];
 
-    library[0].setBookDetails("C++ Primer", "Stanley Lippman", "105", "2024-01-01");
-    library[1].setBookDetails("Effective C++", "Scott Meyers", "102", "2024-02-10");
-    library[2].setBookDetails("Clean Code", "Robert Martin", "101", "2024-03-15");
-    library[3].setBookDetails("The Mythical Man-Month", "Fred Brooks", "104", "2024-04-20");
-    library[4].setBookDetails("Code Complete", "Steve McConnell", "103", "2024-05-25");
+    // Create Author objects
+    Author a1("Stanley Lippman");
+    Author a2("Scott Meyers");
+    Author a3("Robert Martin");
+    Author a4("Fred Brooks");
+    Author a5("Steve McConnell");
 
-    // Sort the book data by ISBN 
+    // Assign books with Author objects
+    library[0].setBookDetails("C++ Primer", a1, "105", "2024-01-01");
+    library[1].setBookDetails("Effective C++", a2, "102", "2024-02-10");
+    library[2].setBookDetails("Clean Code", a3, "101", "2024-03-15");
+    library[3].setBookDetails("The Mythical Man-Month", a4, "104", "2024-04-20");
+    library[4].setBookDetails("Code Complete", a5, "103", "2024-05-25");
+
+    // Sort books by ISBN
     sortBookData(library, SIZE);
 
     string inputISBN;
@@ -85,28 +105,30 @@ int main() {
     cout << "Welcome to the Community Library System" << endl;
     cout << "---------------------------------------" << endl;
 
-    // Main Application Loop [cite: 34-37]
     while (true) {
         cout << "\nCurrent Library Collection:" << endl;
-        cout << left << setw(10) << "ISBN" << setw(25) << "Title" << setw(20) << "Author" << "Status" << endl;
+        cout << left << setw(10) << "ISBN" 
+             << setw(25) << "Title" 
+             << setw(20) << "Author" 
+             << "Status" << endl;
+
         for (int i = 0; i < SIZE; i++) {
             library[i].displayBookDetails();
         }
 
-        // Prompt user for ISBN [cite: 35]
         cout << "\nEnter ISBN (or '0' to exit): ";
         cin >> inputISBN;
 
-        // Program terminates when user enters "0" 
         if (inputISBN == "0") {
             break;
         }
 
         bool found = false;
+
         for (int i = 0; i < SIZE; i++) {
             if (library[i].getISBN() == inputISBN) {
                 found = true;
-                
+
                 cout << "Select Action: (1) Borrow (2) Return: ";
                 cin >> action;
 
@@ -114,7 +136,6 @@ int main() {
                     if (library[i].borrowBook()) {
                         cout << "Success: You have borrowed the book." << endl;
                     } else {
-                        // Error message if unavailable [cite: 36]
                         cout << "Error: This book is currently unavailable." << endl;
                     }
                 } else if (action == 2) {
